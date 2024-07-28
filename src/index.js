a9ddb2dbbdc883ae8889d28b81bad9141e52a111
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2024-07-28 17:12:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-/* import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
- */
-
 import React from 'react';
 import { StrictMode } from 'react';
 import App from './components/App';
@@ -22,7 +10,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/anisoara1/CarboCleaning">
+    <BrowserRouter basename="/CarboCleaning">
       <App />
     </BrowserRouter>
   </StrictMode>
